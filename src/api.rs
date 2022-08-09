a84f0ca5c92ef7f61fdc7be39956cf3b292d6ee9
uzaaft <muaf@nmbu.no>
2022-08-09 20:52:17 +0200

    feat: Add post request

--- src/api.rs
+++ src/api.rs
@@ -1,3 +1,4 @@
+use reqwest::StatusCode;
 use serde::de::DeserializeOwned;
 
 use crate::{client::Client, error::Error};
@@ -5,9 +6,30 @@ use crate::{client::Client, error::Error};
 pub mod account;
 
 impl Client {
+    /// Generic get request
     fn get<T: DeserializeOwned>(&self, path: &str) -> Result<T, Error> {
         let url = format!("{}/{}", self.base_url, path);
         let r = self.client.get(&url).send()?.json::<T>()?;
         Ok(r)
+    }
+
+    /// Generic post request
+    fn post<T: DeserializeOwned>(&self, path: &str, body: String) -> Result<T, Error> {
+        let url = format!("{}/{}", self.base_url, path);
+        let r = self.client.post(&url).body(body).send()?.json::<T>()?;
+        Ok(r)
+    }
+
+    fn response_handler<T: DeserializeOwned>(
+        &self,
+        response: reqwest::blocking::Response,
+    ) -> Result<T, Error> {
+        match response.status() {
+            StatusCode::OK => Ok(response.json::<T>()?),
+            s => {
+                let message = s.to_string();
+                Err(Error::Str(message))
+            }
+        }
     }
 }
