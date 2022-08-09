ac654ea0c259961261f0d407f630603fa393ec8e
uzaaft <muaf@nmbu.no>
2022-08-09 20:28:11 +0200

    fix: Fix error messages

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,8 @@
+use std::borrow::Cow;
+
 /// In general, an error will be returned in the following format.
 /// Lemons docks: [Error Handling](https://docs.lemon.markets/error-handling)
-use reqwest::Error as ReqwestError;
+use reqwest::{Error as ReqwestError, StatusCode};
 use serde_json::Error as JsonError;
 use thiserror::Error;
 
@@ -13,10 +15,24 @@ pub enum Error {
         ReqwestError,
     ),
 
+    #[error("Test")]
+    InternalServerError,
+
     #[error("Encountered an Json related error")]
-    Json(
-        #[from]
-        #[source]
-        JsonError,
-    ),
+    Json(#[from] JsonError),
+
+    #[error("HTTP Error {0}")]
+    Http(StatusCode),
+
+    #[error("Service Unavailable")]
+    ServiceUnavailable,
+
+    #[error("Unauthorized")]
+    Unauthorized,
+
+    #[error("Bad Request")]
+    BadRequest,
+
+    #[error("{0}")]
+    Str(String),
 }
