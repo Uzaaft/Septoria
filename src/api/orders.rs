1df4b624c5afca3d83c868f44f76194377128111
jakeriksen <jakeriksen@gmail.com>
2022-08-15 18:08:22 +0200

    chore(orders): Add docs to struct

--- src/api/orders.rs
+++ src/api/orders.rs
@@ -32,22 +32,39 @@ pub struct OrderPlacingResponse<T> {
 #[derive(Serialize, Deserialize, Debug)]
 // The struct for placing an order - the results response of the request
 pub struct OrderResults {
+    /// Timestamp for when you created your order
     pub created_at: DateTime<Utc>,
+    /// Unique Identification number for your order
     pub id: String,
+    /// The status of your order
     pub status: String,
+    /// Regulatory information about the order
     pub regulatory_information: Option<RegulatoryInformation>,
+    /// Where the order was placed
     pub isin: Option<String>,
+    /// Expiry date of the order
     pub expires_at: Option<String>,
+    /// The type of order you placed (buy or sell)
     pub side: Option<OrderType>,
+    /// The quantity of the order you placed
     pub quantity: Option<i64>,
+    /// The stop_price of the order you placed
     pub stop_price: Option<String>,
+    /// The limit_price of the order you placed
     pub limit_price: Option<String>,
+    /// The venue of the order you placed
     pub venue: Option<String>,
+    /// The estimated price of the order you placed
     pub estimated_price: Option<i64>,
+    /// Notes about your order
     pub notes: Option<String>,
+    /// Idempotency for the request
     pub idempotency: Option<String>,
+    /// Charges for the order request
     pub charge: Option<i64>,
+    // Charges for the order request
     pub chargeable_at: Option<String>,
+    /// Key creation identifier for the order request
     pub key_creation_id: Option<String>,
 }
 
@@ -113,10 +130,7 @@ mod test {
             quantity: 1,
             venue: Some("XMUN".to_string()),
         };
-        dbg!(&body);
-        
         let resp = client.post_an_order(body).unwrap();
-        dbg!(&resp);
         assert_eq!(resp.status, "ok");
     }
 }
