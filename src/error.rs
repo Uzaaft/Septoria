6564e1ec782a0f43b5d092a1d066bcca2df521a3
uzaaft <muaf@nmbu.no>
2022-09-04 20:21:19 +0200

    feat: Add better error support. TODO: Rewrite all usage of strings in error to leverage the new struct

--- src/error.rs
+++ src/error.rs
@@ -2,6 +2,7 @@
 /// Lemons docks: [Error Handling](https://docs.lemon.markets/error-handling)
 use reqwest::{Error as ReqwestError, StatusCode};
 use serde_json::Error as JsonError;
+use serde::Deseriali;
 use thiserror::Error;
 
 /// Error type for the library
@@ -26,4 +27,19 @@ pub enum Error {
     /// Error type for other errors
     #[error("{0}")]
     Str(String),
+}
+
+#[derive(Deserialize)]
+pub struct LemonError {
+    time: String,
+    mode: String,
+    status: String,
+    error_code: String,
+    error_message: String,
+}
+
+impl std::fmt::Display for LemonError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "time: {}, mode: {}, status: {}, error_code: {}, error_message: {}", self.time, self.mode, self.status, self.error_code, self.error_message)
+    }
 }
